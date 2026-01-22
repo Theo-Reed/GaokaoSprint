b31d12b7d44a2a3fa1aec7e7ec3ca267ea6c2b28
yeatom <l980831@163.com>
2026-01-22 21:48:58 +0800

    Refactor code structure for improved readability and maintainability

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",  // <=== Enable static export
-  basePath: "/gaokao", // <=== IMPORTANT: Replace 'gaokao' with your repository name if it's not a user site
+  basePath: "/GaokaoSprint", // <=== IMPORTANT: Replace 'gaokao' with your repository name if it's not a user site
   images: {
     unoptimized: true,
   },
